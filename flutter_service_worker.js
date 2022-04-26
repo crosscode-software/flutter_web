b136e44935b37be38bb81da16f2a951ce8597443
crosscode.co.ltd <crosscode.co.ltd@gmail.com>
2022-04-26 15:34:38 +0800

    use web sample

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,17 +5,22 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "287cd8d50c058a2f2dd13d8f32aaf6fe",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/NOTICES": "cc50a4aadb8ca85ad7994cfcf1a38b08",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "3688efe0a39e59781b4f95efbd6b5b62",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "86cfd465cff093cc0e1ae309996b1d4d",
-"/": "86cfd465cff093cc0e1ae309996b1d4d",
-"main.dart.js": "320b7ee54b257850793e876507fc4c86",
+"index.html": "db3d533572a49ad304bb6f1ee7cff7ad",
+"/": "db3d533572a49ad304bb6f1ee7cff7ad",
+"main.dart.js": "2a1452ae0daa7c290a8ff70c6a87ce1e",
 "manifest.json": "7428f3e970bc27ce92c68a33b5aaa27c",
 "version.json": "9866adfa6e7bd32fd681b6c1dc17e897"
 };
@@ -23,8 +28,7 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
+  "main.dart.js",
 "index.html",
 "assets/NOTICES",
 "assets/AssetManifest.json",
