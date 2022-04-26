8656fce98b3248efaba7486c853117ceeb989884
crosscode.co.ltd <crosscode.co.ltd@gmail.com>
2022-04-26 14:18:09 +0800

    use web sample

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,12 +6,18 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "8dcd477128ae68a4bfe22e35d059676b",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"index.html": "31b7ea6a428c08a25f43099c891a72cd",
-"/": "31b7ea6a428c08a25f43099c891a72cd",
-"main.dart.js": "9950a2f366de9ec3096da54e4dde32e7",
-"version.json": "9094aacdae789dccd67fa32109ff1a18"
+"assets/NOTICES": "287cd8d50c058a2f2dd13d8f32aaf6fe",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "86cfd465cff093cc0e1ae309996b1d4d",
+"/": "86cfd465cff093cc0e1ae309996b1d4d",
+"main.dart.js": "320b7ee54b257850793e876507fc4c86",
+"manifest.json": "7428f3e970bc27ce92c68a33b5aaa27c",
+"version.json": "9866adfa6e7bd32fd681b6c1dc17e897"
 };
 
 // The application shell files that are downloaded before a service worker can
